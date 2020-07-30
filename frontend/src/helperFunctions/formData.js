7cd26550d16cccbd4323e219f77346068244db8a
Lunacy Rising <rerirani20@gmail.com>
2020-07-29 21:35:46 -0300

    arreglado pequeño error cuando llegaba un valor null

--- frontend/src/helperFunctions/formData.js
+++ frontend/src/helperFunctions/formData.js
@@ -3,7 +3,7 @@ import { removeHtmlTag } from "./removeHtmlTag";
 export const formData = (obj) => {
     const data = new FormData();
     for ( let key in obj ) {
-      let objData = typeof obj[key] !== "number" ? removeHtmlTag(obj[key]) : obj[key];
+      let objData = typeof obj[key] !== "number" && obj[key] !== null ? removeHtmlTag(obj[key]) : obj[key];
       data.append(key, objData);
     }
     return data
