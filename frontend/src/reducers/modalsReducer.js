2e75e6a185f45d6d76cfc3ce67b85b9b23d21ce7
Lunacy Rising <rerirani20@gmail.com>
2020-08-05 19:12:25 -0300

    agregados casos para los menu de chat

--- frontend/src/reducers/modalsReducer.js
+++ frontend/src/reducers/modalsReducer.js
@@ -4,24 +4,29 @@ import {
     MODAL_REGISTER_OPEN,
     MODAL_REGISTER_CLOSE,
     OPEN_DRAWER,
-    CLOSE_DRAWER
+    CLOSE_DRAWER,
+    OPEN_CHAT_MENU,
+    CLOSE_CHAT_MENU
   } from "../actions/types";
   
   const initialState = {
     openLogin: false,
     openRegister: false,
-    openDrawer: false 
+    openDrawer: false,
+    chatMenuIds: []
   };
   
   const modalsReducer = (state = initialState, action) => {
     switch (action.type) {
       case MODAL_LOGIN_OPEN: {
         return {
+          ...state,
           openLogin: true 
         };
       }
       case MODAL_LOGIN_CLOSE: {
         return {
+          ...state,
           openLogin: false
         };
       };
@@ -32,17 +37,32 @@ import {
       };
       case MODAL_REGISTER_CLOSE: {
         return {
+          ...state,
           openRegister: false
         }
       };
       case OPEN_DRAWER: {
         return {
+          ...state,
           openDrawer: true
         }
       };
       case CLOSE_DRAWER: {
         return {
+          ...state,
           openDrawer: false
+        }
+      };
+      case OPEN_CHAT_MENU: {
+        return {
+          ...state,
+          chatMenuIds: [action.payload, ...state.chatMenuIds]
+        }
+      };
+      case CLOSE_CHAT_MENU: {
+        return {
+          ...state,
+          openChatMenu: false
         }
       };
       default:
