11ae54d85b973fdae5760a7759aa2f254bd47f47
Lunacy Rising <rerirani20@gmail.com>
2020-08-07 21:01:08 -0300

    refactoring nav

--- frontend/src/components/navBar/Navbar.js
+++ frontend/src/components/navBar/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { AppBar, Avatar, Box, Button, IconButton } from "@material-ui/core";
+import { AppBar, Avatar, Box, Button, IconButton, Typography } from "@material-ui/core";
 import MenuIcon from '@material-ui/icons/Menu';
 import { makeStyles } from "@material-ui/core/styles";
 import { Link } from "react-router-dom";
@@ -8,16 +8,22 @@ import { useSelector, useDispatch } from "react-redux";
 import { openDrawer } from "../../actions/modalsActions/drawer";
 import purple from "../../utils/images/purple.svg";
 import DarkModeSwich from "./darkMode/DarkModeSwich";
+import EngBtn from "./navLinks/translationBtns/EngBtn";
+import EsBtn from "./navLinks/translationBtns/EsBtn";
+import PortBtn from "./navLinks/translationBtns/PortBtn";
 import Notifications from "../notifications/Notifications";
 import FriendInvNotification from "../notifications/FriendInvNotification";
 import LoginBtn from "./navLinks/authBtns/LoginBtn"; 
 import RegisterBtn from "./navLinks/authBtns/RegisterBtn";
 import Drawer from "./Drawer";
 import SearchBar from "./SearchBar";
+import { ThemeConsumer } from "styled-components";
 
 const Navbar = () => {
+
+  const { role, isAuthenticated, userName, avatar} = useSelector(store => store.authReducer);
   
-  const useStyles = makeStyles(() => ({ 
+  const useStyles = makeStyles((theme) => ({ 
 
     navBar: {
       position: "relative",
@@ -25,142 +31,125 @@ const Navbar = () => {
       boxShadow: "0 3px 5px 2px #8b70d2",
       display: "flex",
       flexDirection: "row", 
-      height: "10vh",
+      height: "8vh",
       justifyContent: "space-between",
       alignItems: "center",
       "@media(min-width: 768px)" : {
-        justifyContent: "space-around"
+        justifyContent: "space-around",
+        height: "10vh",
      }
     },
-    links: {
-      color: "white",
-      fontWeight: "bold",
-      textTransform: "uppercase",
-      transition: "0.1s ease all",
-      "&:hover": {
-        textDecoration: "none",
-        color: "#8b70d2",
-      }
+    homeLinkContainer: {
+      display: "flex",
+      alignItems: "center",
+      animation: "aside 1.3s ease-out"
     },
     homeLink: {
-      animation: "aside 1.3s ease-out",
       cursor: "pointer",
       width: 45,
       height: 45,
-      marginLeft: 15
+      marginLeft: 15,
+      "@media(min-width: 1024px)" : {
+        marginRight: 280
+      }
     },
-    btn: {
-      display:"flex",
-      alignItems: "center",
-      color: "white",
-      fontWeight: "bold",
-      fontSize: 16,
-      textTransform: "uppercase",
-      transition: ".2s ease-out",
-      marginRight: 10,
-      "&:hover": {
-        textDecoration: "none",
-        color: "#8b70d2",
-        backgroundColor: "transparent"
-      },
+    user:{
+      display: "none",
+      "@media(min-width: 768px)" : {
+        display: "block",
+        marginRight: 80,
+        marginLeft: 5
+     }
     },
     authBtnsContainer: {
-      display: "flex",
-      alignItems: "center",
-      animation: "aside2 1.3s ease-out",
-      "@media(max-width: 768px)" : {
-        display: "none"
-     }
+      display: "none",
+      "@media(min-width: 1024px)" : {
+        display: "flex",
+        alignItems: "center",
+        animation: "aside2 1.3s ease-out",
+      }
+    },
+    langBtnsContainer: {
+      display: "none",
+      "@media(min-width: 1200px)" : {
+        display: !isAuthenticated && "flex",
+        alignItems: "center",
+        animation: "aside2 1.3s ease-out",
+      }
     },
     hamburger: {
+      width: 30,
+      height: 30,
+      color: "white",
+      transition: "0.2s ease-in-out",
       animation: "aside2 1.3s ease-out",
-      "@media(min-width: 769px)" : {
-       display: "none"
-    }
-    },
-    loggedIn: {
-      display: "none",
-      "@media(min-width: 769px)" : {
-        display: "flex"
-     }
-    },
-    loggedInBtnsContainer: {
-      position: "relative",
-      display: "none",
-      alignItems: "center",
-      "@media(min-width: 769px)" : {
-        display: "flex"
-     }
+      "&:hover":{
+        color: theme.palette.primary.main
+      },
+        "@media(min-width: 1200px)" : {
+        display: !isAuthenticated && "none"
+      }
     }
   }));
 
   const classes = useStyles();
   const {
     navBar,
-    links,
+    homeLinkContainer,
     homeLink,
-    btn,
+    user,
     authBtnsContainer,
-    hamburger,
-    loggedIn,
-    loggedInBtnsContainer
+    langBtnsContainer,
+    hamburger
   } = classes; 
-
-  const { role, isAuthenticated, userName, avatar} = useSelector(store => store.authReducer);
 
   const { t, i18n } = useTranslation();
 
-    const changeLang = (language) => {
-    i18n.changeLanguage(language);
-  }
-
   const dispatch = useDispatch();
-
-  const drawerDispatch = () => {
-    dispatch(openDrawer())
-  }
    
   const auth = 
-    <Box className={`${authBtnsContainer} ${loggedIn}`}>
+    <Box className={`${authBtnsContainer}`}>
       <LoginBtn/>
       <RegisterBtn/>
-      <DarkModeSwich/>
     </Box>
 
-  const loggedInBtns = 
-    <Box className={loggedInBtnsContainer}>
-      <Avatar src={avatar} alt="profile pic"/>
-      <Button style={{color: "white", fontWeight: "bold"}} onClick={() => drawerDispatch()}>
-        {userName}
-      </Button>
-      <Notifications/>
-      <FriendInvNotification/>
-    </Box>
+  
+  const langBtns = 
+  <Box className={langBtnsContainer}>
+    <EngBtn/>
+    <EsBtn/>
+    <PortBtn/>
+  </Box>
 
   return (
     <>
       <nav>
         <AppBar className={navBar} position="fixed">
-            <IconButton className={`${links} ${homeLink}`} component={Link} to="/">
-             <img src={purple} width={40} height={40} alt="logo"/>
-            </IconButton>
-            <SearchBar/>
-            <Box>
-            {!isAuthenticated && auth}
-              <Box className={`${btn} ${hamburger}`}>
-                {!isAuthenticated ?
-                <IconButton  style={{color: "white"}} onClick={() => drawerDispatch()}>
-                  <MenuIcon/>
-                </IconButton>
-                :
-                <IconButton style={{width: 40, height: 40}} onClick={() => drawerDispatch()}> 
-                  <Avatar src={avatar} alt="profile pic"/>
-                </IconButton>}
-                {isAuthenticated && <Notifications/>}
-              </Box>
-              {isAuthenticated && loggedInBtns}
-              <Drawer role={role}/>
+            <Box className={homeLinkContainer}>
+              <IconButton className={homeLink} component={Link} to="/">
+                <img src={purple} width={40} height={40} alt="logo"/>
+              </IconButton>
+              <SearchBar/>
             </Box>
+            {langBtns}
+            {!isAuthenticated && auth}
+            <Box style={{ marginRight: 10}}>
+              <IconButton className={hamburger} onClick={() => dispatch(openDrawer())}>
+                  {!isAuthenticated ? <MenuIcon/> : 
+                  <>
+                    <Avatar src={avatar} alt="profile pic"/>
+                    <Typography className={user}>{userName}</Typography>
+                  </>
+                  }
+              </IconButton>
+              {isAuthenticated &&
+                <>
+                  <FriendInvNotification/>
+                  <Notifications/>
+                </>
+              }
+            </Box>
+            <Drawer role={role}/>
         </AppBar>
       </nav>
     </>
