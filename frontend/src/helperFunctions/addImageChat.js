64dfde60d58fb2904f734b6c7fd7fd36514b40f3
Lunacy Rising <rerirani20@gmail.com>
2020-08-21 01:00:47 -0300

    creada funcion para enviar imagenes por chat

---
+++ frontend/src/helperFunctions/addImageChat.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+import { sendMessage } from "../ioEvents/sendChatMessage";
+
+export const addImageChat = async (e,args) => {
+    const { token, messageInfo, saveMessage, socket } = args;
+    const {receiver, sender, avatar, senderId, receiverId} = messageInfo;
+    const image = e.target.files[0];
+    const previewImage = new FormData(); 
+    previewImage.append("image", image);
+    const response = await axios.post("http://localhost:5001/posts/imagePreview", previewImage, { headers: { "auth-token": token, "Content-Type": "multipart/form-data" } }); 
+    const preview = response.data.preview;
+    // test
+    saveMessage({chatMessage : preview, receiver, sender, avatar, senderId, receiverId})
+    sendMessage("chat message", {chatMessage : preview, receiver, sender, avatar, senderId, receiverId}, socket);
+  };
