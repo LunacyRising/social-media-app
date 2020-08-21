02fe165c9475509114b869f01a6a90a24a7ab916
Lunacy Rising <rerirani20@gmail.com>
2020-08-21 01:32:09 -0300

    agregado componente chatboxescontainer, evento connects socket-io

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,9 +1,9 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import { makeStyles } from "@material-ui/core/styles";
-import { Box, Typography, CircularProgress } from "@material-ui/core";
+import { CircularProgress } from "@material-ui/core";
 import Login from "./auth/Login";
 import Register from "./auth/Register";
 import Posts from "./posts/Posts";
@@ -11,10 +11,8 @@ import PostTextArea from "./posts/PostTextArea";
 import NoResults from "./posts/NoResults";
 import SnackbarMessages from "./SnackbarMessages";
 import FriendMenu from "./friends/FriendMenu";
+import ChatBoxesContainer from "./chat/ChatBoxesContainer";
 import { fetchInitialPosts } from "../actions/postsActions/fetchInitialPosts";
-/////////////////
-import { fetchFriends } from "../actions/friendsActions/fetchFriends";
-
 
 const Home = () => {
 
@@ -47,7 +45,11 @@ const Home = () => {
 
   const { messageCode } = useSelector(state => state.messagesReducer);
 
+  const { socket } = useSelector(state => state.ioReducer);
+
   const { postsLoading, postsLoading2, maxResults, skip } = useSelector(state => state.postReducer);
+
+  const { isAuthenticated, userName } = useSelector(state => state.authReducer);
   
   // se llego al limite de resultados
   const maxResultsReached = skip >= maxResults; 
@@ -56,19 +58,20 @@ const Home = () => {
 
   const { t } = useTranslation();
 
-  const { isAuthenticated } = useSelector(state => state.authReducer);
-
   const dispatch = useDispatch()
-
  
   useEffect(() => {
     dispatch(fetchInitialPosts()); 
   },[]);
 
   useEffect(() => {
-    dispatch(fetchFriends()); 
-  },[]);
-  
+    isAuthenticated && socket && socket.emit("connects", userName);
+    return () => {
+        socket.emit("disconect", "usuario desconectado");
+        socket.off()
+    }
+},[])
+
   return (
     <>
       <main className={main}>
@@ -79,6 +82,7 @@ const Home = () => {
             <Login/>
             <Register/>
           {isAuthenticated && <FriendMenu/>} 
+          <ChatBoxesContainer/>
       </main>
       {<SnackbarMessages/>}
       {messageCode === 500 && history.push("/error")}
