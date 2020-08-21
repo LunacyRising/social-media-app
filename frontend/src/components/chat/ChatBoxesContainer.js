39245b10f5988fc34560ba1f1311d946e464eb83
Lunacy Rising <rerirani20@gmail.com>
2020-08-21 00:33:07 -0300

    agregados estilos

--- frontend/src/components/chat/ChatBoxesContainer.js
+++ frontend/src/components/chat/ChatBoxesContainer.js
@@ -1,32 +1,45 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { makeStyles } from "@material-ui/core/styles";
 import { Box } from "@material-ui/core"; 
 import ChatBox from "./ChatBox";
 
-const ChatBoxesContainer = ({ friends }) => { 
+const ChatBoxesContainer = () => { 
 
   const useStyles = makeStyles(() => ({ 
 
     chatBoxesContainer: {
-        position: "absolute",
-        bottom: 0,
-        right: "100%",
+        position: "fixed",
+        top: 0,
+        right: 0,
         display: "flex",
-        flexDirection: "row-reverse",
-        width: 800,
-        pointerEvents: "none"
+        justifyContent: "end",
+        flexDirection: "column",
+        width: "100%",
+        height: "100vh",
+        pointerEvents: "none",
+        zIndex: 1500,
+        "@media(min-width: 768px)" : {
+          top: "initial",
+          bottom: 0,
+          flexDirection: "row-reverse",
+          height: "initial"
+      },
     },
   }));
+  
   const classes = useStyles();
 
   const { chatBoxesContainer } = classes;
 
+  const { chatBoxes } = useSelector(state => state.chatReducer);
+
   return (
     <>
-        <Box className={chatBoxesContainer} disableGutters={true}> 
-            {friends.map(friend =>(
-              <ChatBox userName={friend.userName} avatar={friend.avatar} friendId={friend.friendId}/>
-            ))}
+        <Box className={chatBoxesContainer}> 
+            {chatBoxes.map(box =>(
+              <ChatBox key={box.id} personUserName={box.userName} personId={box.id} personAvatar={box.personAvatar}/>
+             ))}
         </Box> 
     </>
   );
