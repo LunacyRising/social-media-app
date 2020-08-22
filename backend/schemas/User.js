aa345b1e3259f2d1bf8043beb90a1fac2c34dc15
Lunacy Rising <rerirani20@gmail.com>
2020-08-22 00:58:41 -0300

    agregado isOnline

--- backend/schemas/User.js
+++ backend/schemas/User.js
@@ -48,6 +48,10 @@ const userSchema = new mongoose.Schema({
     type: Boolean,
     default: "false"
   },
+  isOnline: {
+    type: Boolean,
+    default: "false"
+  },
   createdAt: {
     type: Date,
     default: Date.now
