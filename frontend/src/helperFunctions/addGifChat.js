6852b140d5f2f0b7c8f6763a67b80dbbf95e0bde
Lunacy Rising <rerirani20@gmail.com>
2020-08-23 00:45:55 -0300

    agregado valor faltante sendedat

--- frontend/src/helperFunctions/addGifChat.js
+++ frontend/src/helperFunctions/addGifChat.js
@@ -3,9 +3,9 @@ import { sendMessage } from "../ioEvents/sendChatMessage";
 export const addGifChat = (args) => {
     console.log("args", args)
     const { messageInfo, saveMessage, setGifstMenuOpen, socket, gif, title } = args;
-    const {receiver, sender, avatar, senderId, receiverId} = messageInfo;
+    const {receiver, sender, avatar, senderId, receiverId, sendedAt} = messageInfo;
     //dispatch(updateQuery(null));
     setGifstMenuOpen(false);
-    saveMessage({chatMessage : gif, receiver, sender, avatar, senderId, receiverId})
-    sendMessage("chat message", {chatMessage : gif, receiver, sender, avatar, senderId, receiverId}, socket);
+    saveMessage({chatMessage : gif, receiver, sender, avatar, senderId, receiverId, sendedAt})
+    sendMessage("chat message", {chatMessage : gif, receiver, sender, avatar, senderId, receiverId, sendedAt}, socket);
   }
