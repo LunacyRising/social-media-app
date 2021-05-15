2f62071bdc188643394ce017c7dca39e56480fd9
LunacyRising <rerirani20@gmail.com>
2021-05-15 03:06:18 -0300

    changed get api-url & removed log

--- frontend/src/helperFunctions/getApiUrl.js
+++ frontend/src/helperFunctions/getApiUrl.js
@@ -1,8 +1,5 @@
 
 
 export const getApiUrl = () => {
-    const developmentApiUrl = process.env.REACT_APP_LOCAL_API_URL;
-    const productionApiUrl = process.env.REACT_APP_PRODUCTION_API_URL;
-
-    return process.env.NODE_ENV === "development" ? developmentApiUrl : productionApiUrl
+    return process.env.REACT_APP_PRODUCTION_API_URL || "http://localhost:5001";
 }
